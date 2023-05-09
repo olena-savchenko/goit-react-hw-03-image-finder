3d3e21240eecbd974a12740e02ae67d4743a9c5b
olena-savchenko <112640422+olena-savchenko@users.noreply.github.com>
2023-05-09 12:33:25 +0300

    delete unnecessary commits

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -13,34 +13,3 @@ export const ImageGalleryItem = ({ image, toggleModal }) => {
     </StyledImageGalleryItem>
   );
 };
-
-/**
- * 
-export const ImageGalleryItem = ({
-  webformatURL,
-  largeImageURL,
-  tags,
-  onClick,
-  onSelectImage,
-}) => {
-  const handleImageClick = () => {
-    onClick();
-    onSelectImage(largeImageURL);
-  };
-
-  return (
-    <GalleryItem className="gallery-item">
-      <GalleryImage src={webformatURL} alt={tags} onClick={handleImageClick} />
-    </GalleryItem>
-  );
-};
-
-ImageGalleryItem.propTypes = {
-  webformatURL: PropTypes.string.isRequired,
-  largeImageURL: PropTypes.string.isRequired,
-  tags: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
-  onSelectImage: PropTypes.func.isRequired,
-};
-
- */
